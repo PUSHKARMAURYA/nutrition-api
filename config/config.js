7b0274a67d6b0fc95cfdb96081d7f90ff3ea100b
Damon McMinn <damon@mccarthyscott.com>
2014-10-29 16:58:46 +0000

    Removed references to OpenShift in config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   app: {
-    port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip: process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+    port: 50000,
+    ip: 'localhost'
   },
   db: {
-    port: process.env.OPENSHIFT_MONGODB_DB_PORT || 27017,
-    host: process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
+    port: 27017,
+    host: 'localhost',
     name: 'nutrition'
   }
 };
