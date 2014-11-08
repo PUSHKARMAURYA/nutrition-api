0db5cb7ec30752e83ae4716a98e2a72aa9959241
Damon McMinn <damon@mccarthyscott.com>
2014-11-08 13:33:20 +0000

    Pagination, limit of 10 results. Provide links to self, next & prev. Return JSON error string linking to github docs on usage. Include source and values in each result.

--- index.js
+++ index.js
@@ -23,8 +23,14 @@ app.get('/food', function(req, res) {
   var url = req.protocol + '://' +
             req.headers.host +
             req.originalUrl;
+  var baseUrl = url.slice(0, url.indexOf('?') + 1);
+            
   var response = {
-    self: url,
+    links: {
+      self: null,
+      next: null,
+      prev: null
+    },
     total_foods: null,
     foods: []
   };
@@ -34,11 +40,9 @@ app.get('/food', function(req, res) {
     var singleWord = (req.query.name.match(':') === null);
     var matchStart = (req.query.match_start === 'true');
 
-    if (req.query.name.length < 3) {
-      response.error = 'Searches must be 3 characters or greater.';
-      res
-        .status(400)
-        .send(JSON.stringify(response));
+    if (req.query.name.length < 1) {
+      response.error = 'Searches must be at least 1 character or more.';
+      res.status(400).send(JSON.stringify(response));
     } else if (singleWord && matchStart) {
       query.name = {
         $regex: '^' + req.query.name,
@@ -54,16 +58,43 @@ app.get('/food', function(req, res) {
   }
 
   if (!res.headersSent) {
-    var fields = {_id: false};
-    var options = {limit: req.query.pagination, skip: 0};
-    // make cursor here
+    var projection = {
+      _id: false,
+      ndb_id: false,
+    };
+
+    /* pagination limit */
+    var limit = 10;
+    var pageNum = req.query.page || 1;
+    var stop = limit*pageNum;
+    var start = limit*(pageNum-1);
+
     db.collection('food')
-      .find(query, fields)
+      .find(query, projection)
       // use .each -- why build array then iterate it?
       .toArray(function(err, data) {
         if (err) throw err;
+        var totalPages = Math.ceil(data.length/limit);
         response.total_foods = data.length;
-        data.forEach(function(food) {
+        response.total_pages = totalPages;
+
+        if (req.query.page) {
+          response.links.self = url;
+        } else {
+          response.links.self = url + '&page=' + pageNum;
+        }
+        if (pageNum < totalPages) {
+          response.links.next = baseUrl + 'name=' + req.query.name + '&page=' + (Number(pageNum) + 1);
+        } else {
+          delete response.links.next;
+        }
+        if ((pageNum - 1) === 0) {
+          delete response.links.prev;
+        } else {
+          response.links.prev = baseUrl + 'name=' + req.query.name + '&page=' + (Number(pageNum) - 1);
+        }
+
+        data.slice(start, stop).forEach(function(food) {
           response.foods.push(food);
         });
         res
@@ -90,13 +121,8 @@ app.get('/food', function(req, res) {
 });
 
 app.get('*', function(req, res) {
-  res
-    .status(404)
-    .send(jade.renderFile(
-      __dirname + '/views/usage.jade',
-      {url: req.protocol + '://' + req.headers.host}
-    ))
-    .end();
+  var msg = 'Bad request. Visit https://github.com/damonmcminn/nutrition-api for instructions on use';
+  res.status(404).send(JSON.stringify(msg)).end();
 });
 
 app.listen(config.app.port, config.app.ip);
